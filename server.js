316f116122ea8d625710e2b1a6c39d9be5fb8a1c
Ryan Ziegenfus <thewebcoderz@gmail.com>
2019-11-14 16:55:29 -0500

    offer was undefined

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ const express = require('express')
 const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 3000
 
 app.use(express.static(__dirname + "/public"))
 let clients = 0
 
-io.on('connection', function(socket){
+io.on('connection', function (socket){
     socket.on("NewClient", function() {
         if (clients < 2) {
             if (clients == 1) {
-                this.emit(CreatePeer)
+                this.emit('CreatePeer')
             }
         }
         else
@@ -30,11 +30,13 @@ function Disconnect() {
 }
 
 function SendOffer(offer) {
-    this.broadcast.emit("BackOffer", offer)
+    console.log('SendOffer')
+    return this.broadcast.emit("BackOffer", offer)
 }
 
 function SendAnswer(data) {
-    this.broadcast.emit("BackAnswer", data)
+    console.log('SendAnswer')
+    return this.broadcast.emit("BackAnswer", data)
 }
 
 http.listen(port, () => console.log(`Active on port ${port}`))
